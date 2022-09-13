7977e4bc808b6ee4065465e72dffe60dfb732337
yalo24 <olgarizkovic@gmail.com>
2022-09-13 11:39:40 +0300

    using cod without static method

---
+++ src/Task04.java
@@ -0,0 +1,15 @@
+public class Task04 {
+
+    public static void sum() {
+        int result = 5 + 6;
+        System.out.println(result);
+    }
+
+
+    public static void main(String[] args) {
+        sum();
+
+
+
+    }
+}
--- src/Task06.java
+++ src/Task06.java
@@ -4,7 +4,7 @@ public class Task06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = 10;
-        int a = 1;
+
 
 
         for (int j = 1; j < n; j++) {
