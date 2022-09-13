153991cbb002e821ecc42c36c509cf92e388e336
yalo24 <olgarizkovic@gmail.com>
2022-09-13 11:56:45 +0300

    static method

--- src/Task04.java
+++ src/Task04.java
@@ -1,15 +1,20 @@
 public class Task04 {
 
-    public static void sum() {
-        int result = 5 + 6;
+    public static int sum(int a, int b) {
+        int result = a + b;
+        return (result);
+    }
+
+    public static void print(int result) {
         System.out.println(result);
     }
 
-
     public static void main(String[] args) {
-        sum();
+        int result = sum(6, 7);
+        print(result);
 
 
 
     }
 }
+
